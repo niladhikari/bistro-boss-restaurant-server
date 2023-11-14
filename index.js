a978bc4f535bb1d2a8453157036a8c1ba8216fb3
niladhikari <niladhikari445@gmail.com>
2023-11-14 11:59:04 +0600

    add the delete method in the cart collection when the client side delete the item ,the db also delete the item in the db

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcmyfrv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,7 @@ async function run() {
 
     //cart collection
 
+    // add the item email ways in the database
     app.get('/carts', async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -42,10 +43,18 @@ async function run() {
       res.send(result);
     });
 
+    //for the add the item in the database
     app.post('/carts',async(req,res)=>{
        const cartItem = req.body;
        const result = await cartCollection.insertOne(cartItem);
        res.send(result);
+    })
+    // for the delete item in the database
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
     //get operation for the menu data pass the client side
