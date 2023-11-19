305d813d630f27b36da941b5948abd3bc6f43af9
niladhikari <niladhikari445@gmail.com>
2023-11-19 21:16:27 +0600

    add the menu update delete method and add the get item for the get method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -159,6 +160,21 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
+    });
+
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount,'what the problem about hat');
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     //get operation for the menu data pass the client side
